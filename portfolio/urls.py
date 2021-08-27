88b1745676fdf3cd1bcc4daeb68af555afc2b3cc
janiomjunior <mend0152@algonquinlive.com>
2021-08-27 09:55:13 -0400

    more commits

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from jobs.views import nick
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', nick, name='nick'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
